f7b34735d6957b631cc44db81ccac64b00938cab
genewoo <genewoo@gmail.com>
2015-07-12 05:03:22 +0800

    Add basic functionality, will try it on linux

--- lib/multi_ping.rb
+++ lib/multi_ping.rb
@@ -1,5 +1,74 @@
 require "multi_ping/version"
+require 'thread'
 
 module MultiPing
   # Your code goes here...
+
+  DEFAULT = {
+              count: 10,
+              size: 56,
+              parallel: 2
+            }
+
+  class Processor
+    def initialize(options={})
+      @count = options.fetch :count, MultiPing::DEFAULT[:count]
+      @size = options.fetch :size, MultiPing::DEFAULT[:size]
+      @parallel = options.fetch :parallel, MultiPing::DEFAULT[:parallel]
+      @hosts = options.fetch :hosts, []
+      @list_only = options.fetch :list_only, false
+      @results = {}
+      @queue = Queue.new
+      @hosts.each { |host| @queue << host }
+    end
+
+
+    def process
+      workers = (0..@parallel.to_i).map do
+        Thread.new do
+          begin
+            while host = @queue.pop(true)
+              host = host.gsub("\n", '')
+              # puts "ping -c #{@count} -s #{@size} #{host} || echo ERROR"
+              @results[host] = `ping -c #{@count} -s #{@size} #{host} || echo ERROR`.split("\n")
+            end
+          rescue ThreadError
+          end
+        end
+      end
+      workers.map(&:join)
+      self
+    end
+
+    def report
+      sorted_result.each do |server, result|
+        puts "#{server}"
+
+        unless @list_only
+          if result[0] =~ /ERROR/
+            puts "SOMETHING WRONG"
+            puts "ERROR"
+          else
+            puts result[-2..-1]
+          end
+        end
+      end
+    end
+
+    def sorted_result
+      percentage_regexp = /(\d*\.\d*)%/
+      avg_regexp = /\/(\d*\.\d*).*ms/
+      @results.sort_by { |server, result|
+        if result[0] =~ /ERROR/
+          - 100 * 3000
+        else
+          percentage = percentage_regexp.match(result[-2])[1]
+          avg = avg_regexp.match(result[-1])[1]
+          # p [percentage,avg]
+          x = (100 - percentage.to_f) * avg.to_f
+          x
+        end
+      }
+    end
+  end
 end
--- multi_ping.gemspec
+++ multi_ping.gemspec
@@ -11,13 +11,13 @@ Gem::Specification.new do |spec|
 
   spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
   spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.homepage      = "https://github.com/genewoo/multi_ping"
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org by setting 'allowed_push_host', or
   # delete this section to allow pushing this gem to any host.
   if spec.respond_to?(:metadata)
-    spec.metadata['allowed_push_host'] = "TODO: Set to 'http://mygemserver.com'"
+    # spec.metadata['allowed_push_host'] = "TODO: Set to 'http://mygemserver.com'"
   else
     raise "RubyGems 2.0 or newer is required to protect against public gem pushes."
   end
